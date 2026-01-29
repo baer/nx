bb99d434a0ae976e8d1a422488931c15e24b28e2
Louie Weng <56288712+lourw@users.noreply.github.com>
2026-01-29 15:21:04 -0800

    chore(gradle): apply suggestion
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- packages/gradle/src/executors/gradle/build-gradle-args.ts
+++ packages/gradle/src/executors/gradle/build-gradle-args.ts
@@ -17,7 +17,7 @@ export function filterUnparsedOverrides(unparsedOverrides: string[]): string[] {
 export function buildGradleArgs(options: GradleExecutorSchema): string[] {
   let args =
     typeof options.args === 'string'
-      ? options.args.trim().split(' ')
+      ? options.args.trim().split(' ').filter(arg => arg.length > 0)
       : Array.isArray(options.args)
         ? options.args
         : [];
